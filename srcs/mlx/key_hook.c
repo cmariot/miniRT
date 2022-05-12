1ff584d385317313732f27b9bc824e5c9ef7d862
Charles VM UBUNTU <contact@charles-mariot.fr>
2022-05-12 08:45:11 +0200

    Commit pour pull sur VM

--- srcs/mlx/key_hook.c
+++ srcs/mlx/key_hook.c
@@ -6,14 +6,14 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 11:26:42 by cmariot           #+#    #+#             */
-/*   Updated: 2022/05/01 01:22:19 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/05/01 01:39:03 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-#define ESC_KEY 53		// MacOS
-//# define ESC_KEY 65307	// Ubuntu
+//#define ESC_KEY 53		// MacOS
+#define ESC_KEY 65307	// Ubuntu
 
 int	key_hook(int keycode, t_scene *scene)
 {
