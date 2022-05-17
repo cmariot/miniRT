ee6428121890d02a528efa5412728b858e8eff81
rballage <rballage@student.42.fr>
2022-05-17 17:53:18 +0200

    FOV fixed

--- srcs/parsing/new_camera.c
+++ srcs/parsing/new_camera.c
@@ -6,28 +6,27 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 09:16:17 by cmariot           #+#    #+#             */
-/*   Updated: 2022/05/17 16:33:56 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/05/17 17:51:44 by rballage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int	new_camera(t_cam *camera, char **array)
+int	new_camera(t_cam *cam, char **array)
 {
-	camera->print = &print_camera;
+	cam->print = &print_camera;
 	if (ft_arraylen(array) != 4)
-		return (rt_error("Invalid camera declaration."));
-	if (set_position(&(camera->position), array[1]))
-		return (rt_error("Syntax error : Camera position"));
-	if (set_direction(&(camera->direction), array[2]))
-		return (rt_error("Syntax error : Camera direction"));
-	if (set_double(&(camera->fov_horizontal), array[3], 0.0, 180.0))
-		return (rt_error("Syntax error : Camera FOV"));
-	camera->screen_width = round(SCREEN_WIDTH);
-	camera->screen_height = round(camera->screen_width / 1.6);
-	camera->constant_x = 0.5 - camera->screen_width / 2.0;
-	camera->constant_y = 0.5 - camera->screen_height / 2.0;
-	camera->constant_z = -(camera->screen_width
-			/ (2.0 * tan(camera->fov_horizontal / M_PI * 90.0)));
+		return (rt_error("Invalid cam declaration."));
+	if (set_position(&(cam->position), array[1]))
+		return (rt_error("Syntax error : cam position"));
+	if (set_direction(&(cam->direction), array[2]))
+		return (rt_error("Syntax error : cam direction"));
+	if (set_double(&(cam->fov_horizontal), array[3], 0.0, 180.0))
+		return (rt_error("Syntax error : cam FOV"));
+	cam->screen_width = round(SCREEN_WIDTH);
+	cam->screen_height = round(cam->screen_width / 1.6);
+	cam->constant_x = 0.5 - cam->screen_width / 2.0;
+	cam->constant_y = 0.5 - cam->screen_height / 2.0;
+	cam->constant_z = cam->screen_width / (2 * tan((cam->fov_horizontal * M_PI / 180.0) * 0.5 ));
 	return (0);
 }
--- srcs/raytracer/ray_generator.c
+++ srcs/raytracer/ray_generator.c
@@ -6,24 +6,38 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 16:25:43 by cmariot           #+#    #+#             */
-/*   Updated: 2022/05/17 13:55:17 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/05/17 17:50:50 by rballage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
 /*
- * Origine   : Camera 
+ * Origine   : Camera
  * Direction : Pixel (x ; y)
  */
+
+t_v3 get_direction(t_cam *cam, double x, double y)
+{
+	t_v3	dir;
+	double	a;
+	double	b;
+	static double	c;
+
+	a = x + cam->constant_x;
+	b = y + cam->constant_y;
+	c = cam->constant_z;
+	dir.x = 1 * a + 0 * b + 0 * c;
+	dir.y = 0 * a + 1 * b + 0 * c;
+	dir.z = 0 * a + 0 * b + -1 * c;
+	return (dir);
+}
 
 t_ray	ray_generator(t_cam	*camera, double x, double y)
 {
 	t_ray	ray;
 
 	ray.position = camera->position;
-	ray.direction.x = x + camera->constant_x;
-	ray.direction.y = y + camera->constant_y;
-	ray.direction.z = camera->constant_z;
+	ray.direction = get_direction(camera, x, y);
 	return (ray);
 }
