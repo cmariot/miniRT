5f39dfdbf4303a311dae5e45ee1ea3f72e1268a4
rballage <rballage@student.42.fr>
2022-05-30 12:58:43 +0200

    timer added

--- miniRT_bonus/srcs/raytracer/raytracer.c
+++ miniRT_bonus/srcs/raytracer/raytracer.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "miniRT.h"
+#include <time.h>
 
 /*
  * Générer un rayon partant de la camera en direction de chaque pixel de l'écran,
@@ -27,9 +28,11 @@ void	raytracer(t_obj_list *obj_list, t_cam *camera, t_mlx *mlx)
 	int		color;
 	double	x;
 	double	y;
+	double start_time;
 
-	translate_all(obj_list, camera);
+	start_time = (double)clock();
 	lookat(obj_list->camera.direction, obj_list->camera.matrix);
+	translate_all(obj_list, camera);
 	y = 0;
 	while (y < camera->screen_height)
 	{
@@ -44,4 +47,6 @@ void	raytracer(t_obj_list *obj_list, t_cam *camera, t_mlx *mlx)
 		y++;
 	}
 	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->image.img, 0, 0);
+	double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+	printf("Done in %f seconds\n", elapsed_time);
 }
