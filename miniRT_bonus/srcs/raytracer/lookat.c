ee8bbda214d97364748c70e20bc6147293547c7c
Charles Mariot <contact@charles-mariot.fr>
2022-05-31 17:50:36 +0200

    AutoSave

--- miniRT_bonus/srcs/raytracer/lookat.c
+++ miniRT_bonus/srcs/raytracer/lookat.c
@@ -6,7 +6,7 @@
 /*   By: rballage <rballage@student.42.fr>            +#+  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 19:14:56 by rballage           #+#    #+#            */
-/*   Updated: 2022/05/20 19:16:12 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/05/31 17:45:58 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	lookat(t_v3 cam_dir, double *m)
 	t_v3	up;
 
 	placeholder.x = 0;
-	placeholder.y = 1;
+	placeholder.y = -1;
 	placeholder.z = 0;
 	if (cam_dir.x == 0 && cam_dir.z == 0)
 		placeholder.z = 0.000001;
